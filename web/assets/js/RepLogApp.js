5b8be65295dd1db414e253fda9432f53d8a63052
Manu <jgonzalezcantarero@gmail.com>
2019-01-21 09:45:43 +0100

    The for of loop episode ( chapter 2 )

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -35,9 +35,9 @@
             $.ajax({
                 url: Routing.generate('rep_log_list')
             }).then((data) => {
-                $.each(data.items, (key, repLog) => {
+                for (let repLog of data.items) {
                     this._addRow(repLog);
-                });
+                }
             });
         }
 
@@ -70,9 +70,9 @@
 
             const $form = $(e.currentTarget);
             const formData = {};
-            $.each($form.serializeArray(), (key, fieldData) => {
+            for (let fieldData of $form.serializeArray()) {
                 formData[fieldData.name] = fieldData.value
-            });
+            }
             this._saveRepLog(formData)
                 .then((data) => {
                     this._clearForm();
@@ -85,7 +85,7 @@
         _mapErrorsToForm(errorData) {
             const $form = this.$wrapper.find(RepLogApp._selectors.newRepForm);
 
-            $form.find(':input').each((index, element) => {
+            for (let element of $form.find(':input')) {
                 const fieldName = $(element).attr('name');
                 const $wrapper = $(element).closest('.form-group');
                 if (!errorData[fieldName]) {
@@ -97,7 +97,7 @@
                 $error.html(errorData[fieldName]);
                 $wrapper.append($error);
                 $wrapper.addClass('has-error');
-            });
+            }
         }
 
         _removeFormErrors() {
@@ -191,9 +191,9 @@
 
         static _calculateWeights($elements) {
             let totalWeight = 0;
-            $elements.each((index, element) => {
+            for (let element of $elements) {
                 totalWeight += $(element).data('weight');
-            });
+            }
             return totalWeight;
         }
      }
