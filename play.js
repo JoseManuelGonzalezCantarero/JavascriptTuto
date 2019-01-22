e3dc02e0bbe2e28cd76b7492f6c00bd17ab36684
Manu <jgonzalezcantarero@gmail.com>
2019-01-22 10:59:14 +0100

    Array, set and ES2016 episode ( chapter 2 )

--- play.js
+++ play.js
@@ -1,14 +1,6 @@
-let foods = new WeakMap();
-foods.set(['italian'], 'gelato');
-foods.set(['mexican'], 'tortas');
-foods.set(['canadian'], 'poutine');
+let foods = new Set();
+foods.add('gelato');
+foods.add('tortas');
+foods.add('gelato');
 
-let southernUsStates = ['Tennessee', 'Kentucky', 'Texas'];
-foods.set(southernUsStates, 'hot chicken');
-southernUsStates = null;
-
-console.log(
-    foods.get(['italian']),
-    foods.get(southernUsStates),
-    foods.size
-);
+console.log(foods);
--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -12,7 +12,8 @@
 
         constructor($wrapper) {
             this.$wrapper = $wrapper;
-            HelperInstances.set(this, new Helper(this.$wrapper));
+            this.repLogs = [];
+            HelperInstances.set(this, new Helper(this.repLogs));
 
             this.loadRepLogs();
 
@@ -40,6 +41,7 @@
                 for (let repLog of data.items) {
                     this._addRow(repLog);
                 }
+                console.log(this.repLogs, this.repLogs.includes(data.items[0]));
             });
         }
 
@@ -118,9 +120,13 @@
         _addRow(repLog) {
             // let {id, itemLabel, reps, totallyMadeUpKey = 'whatever!'} = repLog;
             // console.log(id, itemLabel, reps, totallyMadeUpKey);
+            this.repLogs.push(repLog);
 
             const html = rowTemplate(repLog);
-            this.$wrapper.find('tbody').append($.parseHTML(html));
+            const $row = $($.parseHTML(html));
+            // store the repLogs index
+            $row.data('key', this.repLogs.length - 1);
+            this.$wrapper.find('tbody').append($row);
 
             this.updateTotalWeightLifted();
         }
@@ -160,6 +166,12 @@
                 method: 'DELETE'
             }).then(() => {
                 $row.fadeOut('normal', () => {
+                    // we need to remove the repLog from this.repLogs
+                    // the "key" is the index to this repLog on this.repLogs
+                    this.repLogs.splice(
+                        $row.data('key'),
+                        1
+                    );
                     $row.remove();
                     this.updateTotalWeightLifted();
                 });
@@ -171,13 +183,13 @@
      * A "private" object
      */
      class Helper {
-        constructor($wrapper) {
-            this.$wrapper = $wrapper;
+        constructor(repLogs) {
+            this.repLogs = repLogs;
         }
 
         calculateTotalWeight() {
             return Helper._calculateWeights(
-                this.$wrapper.find('tbody tr')
+                this.repLogs
             );
         }
 
@@ -191,10 +203,10 @@
             return weight + ' lbs';
         }
 
-        static _calculateWeights($elements) {
+        static _calculateWeights(repLogs) {
             let totalWeight = 0;
-            for (let element of $elements) {
-                totalWeight += $(element).data('weight');
+            for (let repLog of repLogs) {
+                totalWeight += repLog.totalWeightLifted;
             }
             return totalWeight;
         }
