8b01772f5b739b9d076103ab9571ed0d27dee540
Manu <jgonzalezcantarero@gmail.com>
2019-01-24 15:49:35 +0100

    Font awesome & file-loader episode ( chapter 3 )

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -4,6 +4,7 @@ require('bootstrap');
 // make sure the polyfill library is loaded in this main entry
 require('babel-polyfill');
 require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.css');
 require('../css/main.css');
 
 $(document).ready(function() {
