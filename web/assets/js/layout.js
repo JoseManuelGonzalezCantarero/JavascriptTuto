33bdf4d8ec8ec91a29057f8d6ab85d0b8b44421a
Manu <jgonzalezcantarero@gmail.com>
2019-01-24 10:10:50 +0100

    babel-loader options & polyfill episode ( chapter 3 )

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -1,6 +1,8 @@
 'use strict';
 
 require('bootstrap');
+// make sure the polyfill library is loaded in this main entry
+require('babel-polyfill');
 
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,12 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
+                exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader'
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true
+                    }
                 }
             }
         ]
