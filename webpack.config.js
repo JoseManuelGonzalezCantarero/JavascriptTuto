8a8d3fb077f07d9468bb44825e5a8a341cc5a5d5
Manu <jgonzalezcantarero@gmail.com>
2019-01-29 10:14:36 +0100

    Production build episode ( chapter 3 )

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ const sassLoader = {
     }
 };
 
-module.exports = {
+const webpackConfig = {
     entry: {
         rep_log: './assets/js/rep_log.js',
         login: './assets/js/login.js',
@@ -118,3 +118,11 @@ module.exports = {
         headers: { 'Access-Control-Allow-Origin': '*' },
     }
 };
+
+if (process.env.NODE_ENV === 'production') {
+    webpackConfig.plugins.push(
+        new webpack.optimize.UglifyJsPlugin()
+    );
+}
+
+module.exports = webpackConfig;
